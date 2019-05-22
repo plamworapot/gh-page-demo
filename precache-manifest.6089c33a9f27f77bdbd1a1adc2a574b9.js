e12275fe33799ff36a7d2ffbfefc21a553c28b67
Worapot Pengsuk <plamworapot@gmail.com>
2019-05-22 14:16:29 +0700

    Updates

---
+++ precache-manifest.6089c33a9f27f77bdbd1a1adc2a574b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede3a73f986d8574f27d28dc84151b4e",
+    "url": "/gh-page-demo/index.html"
+  },
+  {
+    "revision": "c5ad81adb4793929d098",
+    "url": "/gh-page-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gh-page-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c5ad81adb4793929d098",
+    "url": "/gh-page-demo/static/js/main.f0b66171.chunk.js"
+  },
+  {
+    "revision": "ecb412907a1afecd7b6b",
+    "url": "/gh-page-demo/static/js/runtime~main.f4f4bc0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-page-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
